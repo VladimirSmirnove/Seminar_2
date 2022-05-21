ddc341e7a9b2e2bdd7ff063531496fbf031d031d
Vladimir <vladsmir323@gmail.com>
2022-05-21 07:29:42 +0300

    Программа для отображения 3-й цифры или ее отсутствия заданного числа

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+ 
+Console.Write("Введите число ");
+int Number = int.Parse(Console.ReadLine());
+string NumberText = Convert.ToString(Number);
+if (NumberText.Length > 2 && Number > 0)
+{
+   Console.WriteLine("Третья цифра этого числа -> " + NumberText[2]);
+}
+else
+if (NumberText.Length > 2 && Number < 0)
+{
+   Console.WriteLine("Третья цифра этого числа -> " + NumberText[3]);
+}
+else
+{
+   Console.WriteLine("Третьей цифры нет");
+}
