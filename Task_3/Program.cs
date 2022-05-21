5c2ebb25968b1fc65138cd344f88b5751f00fbf5
Vladimir <vladsmir323@gmail.com>
2022-05-21 07:35:08 +0300

    Программа для отображения дня недели и подтвреждающая выходной день по введеной цифре

---
+++ Task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа которая на вход принимает цифру, обозначающая день недели, и проверяет, является ли этот день выходным.
+Console.Write("Введите цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+void DayWeek(int dayNumber)
+{
+  if (dayNumber == 6 || dayNumber == 7)
+  {
+      Console.WriteLine($"{dayNumber} -> да, этот день Выходной");
+  }
+  else
+  if (dayNumber < 1 || dayNumber >7)
+  {
+Console.WriteLine("Эта цифра не день недели");
+  }
+  else Console.WriteLine($"{dayNumber} -> нет, это НЕ выходной");
+}
+DayWeek(dayNumber);
+ 
+
+
+
