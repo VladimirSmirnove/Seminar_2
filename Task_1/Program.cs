0dd3d744972931caedb3c28a2e441921c2be94ac
Vladimir <vladsmir323@gmail.com>
2022-05-21 07:21:19 +0300

    Программа для отображения 2-й цифры трехзначного числа

---
+++ Task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число: ");
+int Numberthree = Convert.ToInt32(Console.ReadLine());
+if (Numberthree > 99 && Numberthree < 1000)
+{
+  string stringNumber = Convert.ToString(Numberthree);
+  Console.WriteLine("Вторая цифра этого числа -> " + stringNumber[1]);
+}
+else
+if (Numberthree > -1000 && Numberthree < -99)
+{
+  string stringNumber = Convert.ToString(Numberthree);
+  Console.WriteLine("Вторая цифра этого числа -> " + stringNumber[2]);
+}
+else
+{
+  Console.WriteLine("Число не трехзначное");
+}
